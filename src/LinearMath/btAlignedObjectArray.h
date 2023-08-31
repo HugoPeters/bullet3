de8560a8ca46204e2b3385e91f1c3722af6b03c6
Hugo Peters <mail@hugo.fyi>
2023-08-31 23:53:19 +0200

    remove register

--- src/LinearMath/btAlignedObjectArray.h
+++ src/LinearMath/btAlignedObjectArray.h
@@ -42,7 +42,7 @@ subject to the following restrictions:
 #if __cplusplus > 199711L
 #define BT_REGISTER
 #else
-#define BT_REGISTER register
+#define BT_REGISTER /*register*/
 #endif
 
 ///The btAlignedObjectArray template class uses a subset of the stl::vector interface for its methods
